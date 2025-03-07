eae9bfab7984e732b256bcc0db63028ee14e3908
Lauren Hoeft <laurenhoeft42@gmail.com>
2025-03-07 15:40:44 -0500

    Added support for alien files.  Logging still to be completed.

--- bkmedia.sh
+++ bkmedia.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# TODO: Fix bug in alien scan if no alien files present!
+
 # Global Vars
 input="./locations.cfg"
 line_number=0
@@ -7,6 +9,53 @@ restore_number=""
 location_number=""
 backup_flag=false
 OPTSTRING="BR:L:"
+
+# Function to ensure connection is awake
+# TODO: FIX THIS FUNCTION
+#   $1 - Location Path
+wake_up(){
+	host_only=${1%:*}
+	echo "Waking up $host_only"
+	remaining_attemps=10
+
+	while (( remaining_attemps-- > 0 ))
+	do
+		[[ $(ssh $host_only) == 0 ]] && break
+		sleep 5
+	done
+	echo "Host $host_only is now awake."
+}
+
+# Function to connect to remote and detect/process Alien (.xyzar) files
+#   $1 - Location Number
+#   $2 - Location Path
+#   $3 - Compression Flag
+alien_scan() {
+	echo "$1 - $2 ($3)"
+	host_only=${2%:*}
+	echo "Scanning remote ${host_only} for alien files."
+	ssh $host_only << EOF
+		shopt -s globstar
+		shopt -s nullglob
+		echo "Processing alien files."
+		pattern="**/*.xyzar"
+		if [[ "$3" == "restore" ]]; then
+			pattern="\${pattern}.tar"
+		fi
+		for file in \${pattern}; do
+			echo "Processing alien record \$file"
+			if [[ "$3" == "backup" ]]; then
+				tar -czf "\$file.tar" "\$file"
+			elif [[ "$3" == "restore" ]]; then
+				tar -xvzf "\$file"
+			fi
+		done
+EOF
+	# TODO: Update daily log!
+	echo "Daily log updated."
+
+	echo "Scan complete."
+}
 
 # Function to backup a location
 # Arguments:
@@ -17,9 +66,9 @@ backup_location() {
     # Get most recent backup #
     next_num=$(ls ./backups/$1 | sort -nr | head -n 1)
     next_num=$((next_num + 1))
-    back_loc="./backups/$1/$next_num"
+    back_loc="./backups/$1/$next_num/"
     mkdir -p "$back_loc" # Create folders as needed
-    rsync -ah --info=progress2 "$2" "$back_loc"
+    rsync -ah --exclude=*.xyzar --info=progress2 "$2/" "$back_loc"
     echo "Backup created at $back_loc"
 }
 
@@ -30,7 +79,7 @@ backup_location() {
 #   $3 - Backup number
 restore_location() {
     echo "Restoring backup $3 to location $1: $2"
-    res_loc="./backups/$1/$3"
+    res_loc="./backups/$1/$3/"
     rsync -ah --info=progress2 "$res_loc" "$2"
     echo "Restoration complete for: $1: $2"
 }
@@ -79,6 +128,8 @@ process_locations() {
         ((line_number++))
         if $backup_flag; then
             if [ -z "$location_number" ] || [ "$location_number" -eq "$line_number" ]; then
+            	# wake_up $line
+            	alien_scan "$line_number" "$line" "backup"
                 backup_location "$line_number" "$line"
             fi
         elif [ -n "$restore_number" ]; then
@@ -87,7 +138,9 @@ process_locations() {
                 exit 1
             else
                 if [ "$location_number" -eq "$line_number" ]; then
+                	# wake_up $line
                     restore_location "$line_number" "$line" "$restore_number"
+                    alien_scan "$line_number" "$line" "restore"
                     exit 0 # Exit after restore (only one restore per run allowed).
                 fi
             fi
